af60c5294b60a7c5363f001acae86a5909ee5199
harutoyyy <159074392+harutoyyy@users.noreply.github.com>
2024-06-05 12:55:06 -0700

    s

--- task1.py
+++ task1.py
@@ -7,3 +7,35 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+
+
+
+#!python3
+"""
+Assigning a function to a Button widget
+Getting and Inserting Entry widgets
+"""
+import tkinter as tk
+from tkinter import *
+
+root = Tk()
+root.title("Entry box")
+root.geometry("700x500")
+
+my_entries = []
+
+def something():
+
+    entry_list = ''
+    for entries in my_entries:
+        entry_list = entry_list + str(entries.get()) + '\n'
+        my_label.config(text=entry_list)
+    print(my_entries[0].get())
+
+for x in range(5):
+    my_entry = Entry(root)
+    my_entry.grid(row=0, column=x, pady=20, padx=5)
+    my_entries.append(my_entry)
+
+my_button = Button(root, text="Click Me!", command=something)
+my_button.grid(row=1, column=0, pady=20)
--- task2.py
+++ task2.py
@@ -6,3 +6,16 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+import tkinter as tk
+from tkinter import *
+
+
+win = tk.Tk()
+
+button = Button
+
+first = tk.Entry 
+second = tk.Entry
+
+def hypotenuse():
+    first^2 + second^2
